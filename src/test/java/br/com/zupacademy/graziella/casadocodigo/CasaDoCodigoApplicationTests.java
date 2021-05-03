cfec4168ba68aabaecaf3a8bc6e42ff1950f70e0
Graziella Farias <grazivictoria.08@gmail.com>
2021-05-03 15:05:04 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/graziella/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.graziella.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
